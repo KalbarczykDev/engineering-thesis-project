a54fbd527dce07020f3f98f883082c5223a1d55a
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-10-22 15:35:53 +0200

    remove ExerciseListComponent from app imports

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
-import {ExerciseListComponent} from "./features/exercise/components/exercise-list/exercise-list.component";
 import {RouterOutlet} from "@angular/router";
 import {HeaderComponent} from "./core/layout/header/header.component";
 import {FooterComponent} from "./core/layout/footer/footer.component";
@@ -8,7 +7,7 @@ import {FooterComponent} from "./core/layout/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HttpClientModule, ExerciseListComponent, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [HttpClientModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
