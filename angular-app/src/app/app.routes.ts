298a7f72005bf5b8740913ceb24096440bc6e7b3
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-10-30 13:22:52 +0100

    add profile component; update home layout and routing

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,9 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from "./features/home/home.component";
 import {ExerciseListComponent} from "./features/exercise/components/exercise-list/exercise-list.component";
+import {WorkoutListComponent} from "./features/workout/components/workout-list/workout-list.component";
+import {ProfileComponent} from "./features/profile/components/profile/profile.component";
 
 export const routes: Routes = [
-  {path : '', component: HomeComponent},
-  {path : 'exercises',component: ExerciseListComponent},
-  {path : '**' , redirectTo: ''}
+  {path: '', component: HomeComponent},
+  {path: 'exercises', component: ExerciseListComponent},
+  {path: 'workouts', component: WorkoutListComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: '**', redirectTo: ''}
 ];
