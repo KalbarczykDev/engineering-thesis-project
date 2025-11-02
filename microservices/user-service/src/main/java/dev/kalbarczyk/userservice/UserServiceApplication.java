61eaac4e6e8d8f50c6f03f3aa3e8c8589ff3a5bf
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-02 13:49:17 +0100

    refactor UserServiceApplication; remove unused imports and clean up code

--- microservices/user-service/src/main/java/dev/kalbarczyk/userservice/UserServiceApplication.java
+++ microservices/user-service/src/main/java/dev/kalbarczyk/userservice/UserServiceApplication.java
@@ -2,9 +2,7 @@ package dev.kalbarczyk.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("dev.kalbarczyk")
