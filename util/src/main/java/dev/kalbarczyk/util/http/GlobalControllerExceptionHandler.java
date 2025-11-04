94766c882ec17b7676186cb7f64e44c37b46ae40
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-04 11:39:52 +0100

    feat: add BadRequestException handler; implement exception handling for bad request scenarios in GlobalControllerExceptionHandler

--- util/src/main/java/dev/kalbarczyk/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/dev/kalbarczyk/util/http/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package dev.kalbarczyk.util.http;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import dev.kalbarczyk.api.exceptions.BadRequestException;
 import dev.kalbarczyk.api.exceptions.InvalidInputException;
 import dev.kalbarczyk.api.exceptions.NotFoundException;
 
@@ -19,13 +21,18 @@ class GlobalControllerExceptionHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public @ResponseBody HttpErrorInfo handleBadRequestExceptions(
+            final ServerHttpRequest request, final BadRequestException ex) {
+
+        return createHttpErrorInfo(BAD_REQUEST, request, ex);
+    }
 
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public @ResponseBody HttpErrorInfo handleNotFoundExceptions(
-            final ServerHttpRequest request,
-            final NotFoundException ex
-    ) {
+            final ServerHttpRequest request, final NotFoundException ex) {
 
         return createHttpErrorInfo(NOT_FOUND, request, ex);
     }
@@ -33,17 +40,13 @@ class GlobalControllerExceptionHandler {
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidInputException.class)
     public @ResponseBody HttpErrorInfo handleInvalidInputException(
-            final ServerHttpRequest request,
-            final InvalidInputException ex
-    ) {
+            final ServerHttpRequest request, final InvalidInputException ex) {
+
         return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
     }
 
     private HttpErrorInfo createHttpErrorInfo(
-            final HttpStatus httpStatus,
-            final ServerHttpRequest request,
-            final Exception ex
-    ) {
+            final HttpStatus httpStatus, final ServerHttpRequest request, final Exception ex) {
 
         var path = request.getPath().pathWithinApplication().value();
         var message = ex.getMessage();
