4c2bd08386f90526e873a9593fe09b837c67512a
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-04 14:39:18 +0100

    feat: enhance UserServiceApplicationTests; add user creation, update, retrieval, and deletion tests with improved setup

--- microservices/user-service/src/test/java/dev/kalbarczyk/userservice/UserServiceApplicationTests.java
+++ microservices/user-service/src/test/java/dev/kalbarczyk/userservice/UserServiceApplicationTests.java
@@ -6,14 +6,15 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static reactor.core.publisher.Flux.just;
 
 import dev.kalbarczyk.api.core.user.User;
+import dev.kalbarczyk.userservice.persistence.UserEntity;
 import dev.kalbarczyk.userservice.persistence.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.time.LocalDateTime;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class UserServiceApplicationTests extends MySqlTestBase {
@@ -24,57 +25,134 @@ class UserServiceApplicationTests extends MySqlTestBase {
     @Autowired
     private UserRepository repository;
 
+    private UserEntity savedUser;
+
+
+    @BeforeEach
+    void setupDb() {
+        repository.deleteAll();
+
+        var user = UserEntity.builder().username("username").password("password").email("email").build();
+        savedUser = repository.save(user);
+    }
+
     @Test
     void shouldGetUserById() {
-
-        var userId = 1L;
-
-        postAndVerifyUser(userId, HttpStatus.OK);
-
-        assertTrue(repository.findById(userId).isPresent());
-
-        getAndVerifyUser(userId, HttpStatus.OK).jsonPath("$.userId").isEqualTo(userId);
-
-    }
-
-
-    private WebTestClient.BodyContentSpec getAndVerifyUser(final Long userId, final HttpStatus expectedStatus) {
-        return getAndVerifyUser("/" + userId, expectedStatus);
-    }
-
-    private WebTestClient.BodyContentSpec getAndVerifyUser(final String userIdPath, final HttpStatus expectedStatus) {
-        return client.get()
-                .uri("/users" + userIdPath)
-                .accept(APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isEqualTo(expectedStatus)
-                .expectHeader().contentType(APPLICATION_JSON)
-                .expectBody();
-    }
-
-
-    private WebTestClient.BodyContentSpec postAndVerifyUser(final Long userId, final HttpStatus expectedStatus) {
-        var user = new User(null, "username" + userId, "email" + userId + "@example.com", "secret",
-                LocalDateTime.now().toString(), LocalDateTime.now().toString());
-
-        return client.post()
-                .uri("/users")
-                .bodyValue(user)
-                .accept(APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isEqualTo(expectedStatus)
-                .expectHeader().contentType(APPLICATION_JSON)
-                .expectBody();
-    }
-
-    private WebTestClient.BodyContentSpec deleteAndVerifyUser(final Long userId, final HttpStatus expectedStatus) {
-        return client.delete()
+        var userId = savedUser.getId();
+        client.get()
                 .uri("/users/" + userId)
                 .accept(APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isEqualTo(expectedStatus)
-                .expectBody();
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.userId").isEqualTo(userId)
+                .jsonPath("$.username").isEqualTo(savedUser.getUsername())
+                .jsonPath("$.email").isEqualTo(savedUser.getEmail())
+                .jsonPath("$.password").isEqualTo(savedUser.getPassword())
+                .jsonPath("$.createdAt").isEqualTo(savedUser.getCreatedAt())
+                .jsonPath("$.updatedAt").isEqualTo(savedUser.getUpdatedAt());
+    }
+
+    @Test
+    void shouldThrowWhenUserNotFound(){
+        var userId = savedUser.getId() + 1;
+        client.get()
+                .uri("/users/" + userId)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/users/" + userId)
+                .jsonPath("$.message").isEqualTo("No user found for userId: " + userId);
+    }
+
+    @Test
+    void shouldCreateUser(){
+        var newUser = new User(null, "newUsername", "newEmail@gmail.com", "newPassword", null, null);
+        client.post()
+                .uri("/users")
+                .bodyValue(newUser)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.userId").isNumber()
+                .jsonPath("$.username").isEqualTo("newUsername")
+                .jsonPath("$.email").isEqualTo("newEmail@gmail.com")
+                .jsonPath("$.password").isEqualTo("newPassword")
+                .jsonPath("$.createdAt").isNotEmpty()
+                .jsonPath("$.updatedAt").isNotEmpty();
+    }
+
+    @Test
+    void shouldThrowWhenCreatingInvalidUser(){
+        var newUser = new User(null, null, "mail.com", "n", null, null);
+
+        client.post()
+                .uri("/users")
+                .bodyValue(newUser)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+                .expectHeader().contentType(APPLICATION_JSON);
+
+
     }
 
 
+
+    @Test
+    void shouldUpdateUser() {
+        var userId = savedUser.getId();
+        var updated = new User(userId, "updatedUsername", "updated@gmail.com", "updatedPassword", null, null);
+
+        client.put()
+                .uri("/users/" + userId)
+                .bodyValue(updated)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.userId").isEqualTo(userId)
+                .jsonPath("$.username").isEqualTo("updatedUsername")
+                .jsonPath("$.email").isEqualTo("updated@gmail.com")
+                .jsonPath("$.password").isEqualTo("updatedPassword");
+
+        var entity = repository.findById(userId).orElseThrow();
+        assertTrue(entity.getUsername().equals("updatedUsername"));
+        assertTrue(entity.getEmail().equals("updated@gmail.com"));
+    }
+
+    @Test
+    void shouldThrowWhenUpdatingUser() {
+        var userId = savedUser.getId() + 1;
+        var updated = new User(userId, "wrong", "wrong@mail.com", "pass", null, null);
+
+        client.put()
+                .uri("/users/" + userId)
+                .bodyValue(updated)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/users/" + userId)
+                .jsonPath("$.message").isEqualTo("No user found for userId: " + userId);
+    }
+
+    @Test
+    void shouldDeleteUser() {
+        var userId = savedUser.getId();
+        client.delete()
+                .uri("/users?userId=" + userId)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK);
+
+        assertTrue(repository.findById(userId).isEmpty());
+    }
 }
