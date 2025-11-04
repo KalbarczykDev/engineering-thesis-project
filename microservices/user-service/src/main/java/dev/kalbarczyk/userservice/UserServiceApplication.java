48b42c9624621e9ff0702594faa9b2ac03691d83
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-04 12:09:06 +0100

    feat: enhance application startup logging; log MySQL connection URL on startup

--- microservices/user-service/src/main/java/dev/kalbarczyk/userservice/UserServiceApplication.java
+++ microservices/user-service/src/main/java/dev/kalbarczyk/userservice/UserServiceApplication.java
@@ -1,15 +1,20 @@
 package dev.kalbarczyk.userservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("dev.kalbarczyk")
+@Slf4j
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        var context = SpringApplication.run(UserServiceApplication.class, args);
+        var mysqlUri = context.getEnvironment().getProperty("spring.datasource.url");
+        log.info("Connected to MySQL: {}", mysqlUri);
     }
 
 }
