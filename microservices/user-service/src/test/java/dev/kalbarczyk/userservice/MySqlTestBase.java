f51e9e49b87fbc7b413114b7cd511df5f0d8a331
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-04 13:25:22 +0100

    feat: enhance PersistenceTests; add update, delete, and error handling tests for UserEntity

--- microservices/user-service/src/test/java/dev/kalbarczyk/userservice/MySqlTestBase.java
+++ microservices/user-service/src/test/java/dev/kalbarczyk/userservice/MySqlTestBase.java
@@ -5,8 +5,10 @@ import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.MySQLContainer;
 
 
+@SuppressWarnings("resource")
 public abstract class MySqlTestBase {
 
+    @SuppressWarnings("rawtypes")
     @ServiceConnection
     static final JdbcDatabaseContainer database = new MySQLContainer("mysql:9.2.0").withStartupTimeoutSeconds(300);
 
--- microservices/user-service/src/test/java/dev/kalbarczyk/userservice/PersistenceTests.java
+++ microservices/user-service/src/test/java/dev/kalbarczyk/userservice/PersistenceTests.java
@@ -1,12 +1,10 @@
 package dev.kalbarczyk.userservice;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
 
-import java.time.LocalDateTime;
-import java.util.List;
+
 
 import dev.kalbarczyk.userservice.persistence.UserEntity;
 import dev.kalbarczyk.userservice.persistence.UserRepository;
@@ -14,6 +12,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -58,6 +58,55 @@ public class PersistenceTests extends MySqlTestBase {
         assertEqualsUser(newEntity, foundEntity);
         assertEquals(2, repository.count());
 
+    }
+
+    @Test
+    void update() {
+        savedEntity.setEmail("newEmail@example.com");
+        repository.save(savedEntity);
+
+        var foundEntity = repository.findById(savedEntity.getId()).orElseThrow();
+        assertEquals(1, (long) foundEntity.getVersion());
+        assertEquals("newEmail@example.com", foundEntity.getEmail());
+    }
+
+    @Test
+    void delete() {
+        repository.delete(savedEntity);
+        assertFalse(repository.existsById(savedEntity.getId()));
+    }
+
+
+    @Test
+    void getByUserId() {
+        var foundEntity = repository.findById(savedEntity.getId()).orElseThrow();
+        assertEqualsUser(savedEntity, foundEntity);
+    }
+
+    @Test
+    void duplicateError() {
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            var entity = UserEntity.builder().username("username").email("email@example.com").password("password").build();
+            repository.save(entity);
+        });
+    }
+
+    @Test
+    void optimisticLockError() {
+        var entity1 = repository.findById(savedEntity.getId()).orElseThrow();
+        var entity2 = repository.findById(savedEntity.getId()).orElseThrow();
+
+        entity1.setEmail("new@example.com");
+        repository.save(entity1);
+
+        assertThrows(OptimisticLockingFailureException.class, () -> {
+            entity2.setEmail("new2@example.com");
+            repository.save(entity2);
+        });
+
+        var updatedEntity = repository.findById(savedEntity.getId()).orElseThrow();
+        assertEquals(1, (long) updatedEntity.getVersion());
+        assertEquals("new@example.com", updatedEntity.getEmail());
     }
 
     private void assertEqualsUser(final UserEntity expectedEntity, final UserEntity actualEntity) {
