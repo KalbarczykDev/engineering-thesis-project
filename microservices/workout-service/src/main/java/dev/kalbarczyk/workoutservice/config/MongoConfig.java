5bd47d87c1c3ff536a1fbd2e72fae801cf04f40e
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-07 17:43:13 +0100

    refactor: migrate MongoConfig to workout service and remove unused GridFSBucket bean

--- microservices/workout-service/src/main/java/dev/kalbarczyk/workoutservice/config/MongoConfig.java
+++ microservices/workout-service/src/main/java/dev/kalbarczyk/workoutservice/config/MongoConfig.java
@@ -1,4 +1,4 @@
-package dev.kalbarczyk.profileservice.config;
+package dev.kalbarczyk.workoutservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
