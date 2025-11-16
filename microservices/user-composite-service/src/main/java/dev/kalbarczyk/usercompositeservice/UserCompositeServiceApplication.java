8ec39cf29945ba5552492d2b333dc24666902aad
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-16 13:23:14 +0100

    feat: update application configuration and add load-balanced WebClient

--- microservices/user-composite-service/src/main/java/dev/kalbarczyk/usercompositeservice/UserCompositeServiceApplication.java
+++ microservices/user-composite-service/src/main/java/dev/kalbarczyk/usercompositeservice/UserCompositeServiceApplication.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -29,6 +31,12 @@ public class UserCompositeServiceApplication {
     public Scheduler publishEventScheduler() {
         log.info("Creates a messagingScheduler with connectionPoolSize = {}", threadPoolSize);
         return Schedulers.newBoundedElastic(threadPoolSize, taskQueueSize, "publish-pool");
+    }
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
     }
 
 
