e4e1acbfabac25034f91d4133b8719075eee747a
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-11-16 15:34:17 +0100

    feat: add security configuration for Eureka server with basic authentication

---
+++ spring-cloud/eureka-server/src/main/java/dev/kalbarczyk/springcloud/eurekaserver/SecurityConfig.java
@@ -0,0 +1,49 @@
+package dev.kalbarczyk.springcloud.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    private final String username;
+    private final String password;
+
+    public SecurityConfig(
+            final @Value("${app.eureka-username}") String username,
+            final @Value("${app.eureka-password}") String password
+    ) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        @SuppressWarnings("deprecation")
+        var user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        // Disable CSRF to allow services to register themselves with Eureka
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
