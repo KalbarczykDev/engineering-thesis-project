369250147acb51f6c40e63b325b0fe06e945d94e
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-12-14 21:12:15 +0100

    feat: add ServiceUnavailableException for better error handling

---
+++ api/src/main/java/dev/kalbarczyk/api/exceptions/ServiceUnavailableException.java
@@ -0,0 +1,18 @@
+package dev.kalbarczyk.api.exceptions;
+
+public class ServiceUnavailableException extends RuntimeException {
+    public ServiceUnavailableException() {
+    }
+
+    public ServiceUnavailableException(String message) {
+        super(message);
+    }
+
+    public ServiceUnavailableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceUnavailableException(Throwable cause) {
+        super(cause);
+    }
+}
